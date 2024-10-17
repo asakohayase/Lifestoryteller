56732a244dbddee5243d35c78debcd0c608c0e5f
Asako Hayase <chipmunk.kck@gmail.com>
2024-10-17 12:46:31 -0700

    fix loggers

--- backend/utils/log_config.py
+++ backend/utils/log_config.py
@@ -1,30 +1,33 @@
 import logging
+import sys
 
-
-def setup_logger(name):
+def setup_logger(name: str, level=logging.INFO):
     """
-    Create and return a logger with the given name.
-
+    Set up a logger that only logs to the console.
+    
     :param name: Name of the logger (usually __name__ of the module calling this function)
-    :return: A configured logger instance
+    :param level: Logging level (default is INFO)
+    :return: Configured logger instance
     """
     # Create a logger
     logger = logging.getLogger(name)
-
-    # Set the logging level
-    logger.setLevel(logging.INFO)
+    logger.setLevel(level)
 
     # Create a formatter
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        datefmt="%Y-%m-%d %H:%M:%S"
     )
 
-    # Create a handler (for console output)
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
+    # Create a console handler and set level
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(level)
+    console_handler.setFormatter(formatter)
 
     # Add the handler to the logger
-    logger.addHandler(handler)
+    logger.addHandler(console_handler)
 
     return logger
+
+# Create a main application logger
+main_logger = setup_logger('family_book_app')
